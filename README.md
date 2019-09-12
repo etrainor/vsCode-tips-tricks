54d1474bdf5fccb084295ecf98f26c056bf25c82
Erin Trainor <trainor.erin@gmail.com>
2019-09-11 22:57:06 -0700

    25 percent of list explained

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# vsCode-tips-tricks
+# VS Code Tips and Tricks
+Since starting code 201 at Code Fellows, I have discovered many time saving and useful tools for VS Code and the terminal.
+
+## List of Tricks
+### Split Screens
+If you click the icon in the upper right corner of VS code that looks like a box divided in half vertically it will allow you to view 2 (or more files at the same time);
+
+### Move Code Up or Down without copy/paste
+To move a single line just place your cursor on the line to move and:
+- MAC = Option + up/down arrow
+- PC = Alt + up/down arrow
+
+To move multiple lines highlight all of the lines to be moved and use the same keyboard shortcuts
+
+### Autosave
+To configure your autosave, click the gear in the lower left corner. Select "settings". Under Auto Save change the option to "afterDelay".
+
+This will cause VS Code to auto save your work when you navigate away from VS Code to another window.
+
+### Word Wrap
+If you have long lines of code or text that are flowing out of view on the right of your file, you can toggle word wrap.
+- MAC = Option + Z
+- PC = ALT + Z
+
+You can also permanently turn on word wrap in the settings (search for word wrap)
+
+#### Want to learn more about writing in markdown (.md files)?? 
+This is one of my favorite guides:
+
+https://www.markdownguide.org/basic-syntax/
--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
-'use strict'
+"use strict";
 
-// TODO: DO the things!!!
+//=================================================================================
+
+// Split Screens
+// Sometimes it is useful to see two files side by side simultaneously.
+// Especially when working on HTML/CSS
+
+//=================================================================================
+
+// Move Code Up or Down without copy/paste
+// Frequently you will find that you either want to (or need to) reorder portions of your code.
+// You can do this without having to cut, copy, paste, or re-type!
+
+// var sum = numberOne + numberTwo;
+
+// console.log('The sum of numberOne and numberTwo is: ', sum);
+
+// var numberOne = 1;
+// var numberTwo = 2;
+
+//=================================================================================
+
+// Configure Autosave
+// If you want your files to save automatically when you navigate away from VS Code, you can change the autosave settings.
+
+//=================================================================================
+
+// Word Wrap
+
+// var reallyLongText = 'Cupcake ipsum dolor. Sit amet biscuit chocolate bar icing cake muffin cookie bear claw. Lemon drops marzipan brownie sesame snaps. Powder pudding toffee chocolate bar cheesecake gummies. Carrot cake pudding donut apple pie sugar plum caramels gummi bears dragée. Wafer tart danish jelly beans lollipop wafer pastry soufflé topping. Tiramisu bear claw icing cake dragée. Sweet bonbon topping oat cake liquorice cake sugar plum carrot cake carrot cake. Pie jujubes gummi bears cupcake. Icing chocolate cake powder. Candy dessert donut toffee chocolate bar biscuit. Marshmallow candy canes cotton candy. Pudding liquorice sugar plum caramels brownie cotton candy carrot cake chocolate cake dragée.';
+
+//=================================================================================
+
+// How to know if your eslint is not working
+
+var monkeys = "cute brown furry animals";
+var monkeyFood = bananas
+if(monkeyFood = 'bananas'){
+      console.log('monkeys eat: ',  monkeyFood)
+} 
